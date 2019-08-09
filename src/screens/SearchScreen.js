03b081c2da460ee6dfdc7da9f9428746acb3c3f9
vadkutsen <vadkutsen@gmail.com>
2019-08-09 12:52:30 +0200

    Add useEffect hook

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
@@ -8,27 +8,35 @@ const SearchScreen = () => {
     const [results, setResults] = useState([]);
     const [errorMessage, setErrorMessage] = useState('');
 
-    const searchApi = async () => {
+    const searchApi = async (searchTerm) => {
+        console.log('Hi there!');
         try {
             const response = await yelp.get('/search', {
                 params: {
                     limit: 50,
-                    term,
+                    term: searchTerm,
                     location: 'wroclaw'
                 }
             });
             setResults(response.data.businesses);
         } catch (err) {
-            setErrorMessage('Something went wrong');
+            setErrorMessage('Something went wrong. Please, try again later');
         }
     };
+
+    // Call searchApi when component
+    // is first rendered. BAD CODE!
+    // searchApi('pasta');
+    useEffect(() => {
+        searchApi('pasta');
+    }, []);
 
     return (
         <View>
             <SearchBar
                 term={term}
                 onTermChange={setTerm}
-                onTermSubmit={searchApi}
+                onTermSubmit={() => searchApi(term)}
             />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
             <Text>We have found {results.length} results</Text>
